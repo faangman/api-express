cc1224b1eab65f8ba92cc080015ed861d943d27c
faangman <faang.net@gmail.com>
2024-10-31 14:37:56 -0700

    first endpoint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send(`Hello World!` );
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
